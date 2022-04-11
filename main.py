885ab89ad45ed549e5b265c308b8ec2c9497f863
Oskar <69815@student.ue.poznan.pl>
2022-04-11 10:35:16 +0200

    test dataclasses

--- main.py
+++ main.py
@@ -1,87 +1,85 @@
-from dataclasses import dataclass, astuple
-from operator import itemgetter
+from dataclasses import dataclass, field
 import ast
 
-@dataclass(frozen=True, order=True)
+@dataclass(frozen=True)
 class Ksiazka:
     tytul: str
     autor: str
     rok: int
 
-@dataclass(frozen=True, order=True)
+@dataclass(frozen=False)
+class Czytelnik:
+    imie: str
+    wypozyczone: list = field(default_factory=list)
+
+@dataclass(frozen=True)
 class Biblioteka:
-    ksiazki: dict() # slownik gdzie kluczem jest krotka tytul,autor,rok a wartością liczba egzemplarzy ksiązek 
-    czytelnicy: dict() # zagniezdzony slownik, kazdy czytelnik ma swoj slownik wypozyczen {tytul: 1}
+    ksiazki: dict() = field(default_factory=dict) # slownik gdzie klucz to obiekt klasy Ksiazka a wartosc liczba egzemplarzy
+    czytelnicy: dict() =field(default_factory=dict) # slownik z obiektami klasy Czytelnik
 
 
-    def pobierz_czytelnika(self,imie) -> bool:
-        if imie in self.czytelnicy:
+    def pobierz_czytelnika(self,czytelnik: Czytelnik) -> bool:
+        if czytelnik.imie in self.czytelnicy:
             return True
         else:
             return False
     
-    def sprawdz_stan(self,tytul_ksiazki) -> bool:
+    def sprawdz_stan(self,tytul_ksiazki: str) -> bool:
         for ksiazka, egzemplarze in self.ksiazki.items():
             if ksiazka.tytul == tytul_ksiazki:
                 return egzemplarze > 0
         
         return False
 
-    def ksiazka_po_tytule(self,tytul_ksiazki) -> Ksiazka:
+    def ksiazka_po_tytule(self,tytul_ksiazki: str) -> Ksiazka:
         for ksiazka in self.ksiazki:
             if ksiazka.tytul == tytul_ksiazki:
                 return ksiazka
     
-    def sprawdz_czy_ta_sama(self,czytelnik, tytul) -> bool:
-        if czytelnik in self.czytelnicy:
-            if tytul in self.czytelnicy[czytelnik]:
-                if self.czytelnicy[czytelnik][tytul] == 1:
+    def sprawdz_czy_ta_sama(self,czytelnik: Czytelnik, tytul: str) -> bool:
+        if czytelnik.imie in self.czytelnicy:
+            if tytul in self.czytelnicy[czytelnik.imie]:
                     return True
         return False
     
-    def sprawdz_czy_trzy(self,czytelnik) -> bool:
-        if czytelnik in self.czytelnicy:
-            suma_wypozyczonych = 0
-            for ksiazka,egzemplarze in self.czytelnicy[czytelnik].items():
-                suma_wypozyczonych += egzemplarze
-            return suma_wypozyczonych > 2
+    def sprawdz_czy_trzy(self,czytelnik: Czytelnik) -> bool:
+        if czytelnik.imie in self.czytelnicy:
+            return len(self.czytelnicy[czytelnik.imie]) > 2
 
             
 
 
-    def dodaj_ksiazke(self,ksiazka):
+    def dodaj_ksiazke(self,ksiazka: Ksiazka):
         if ksiazka not in self.ksiazki:
             self.ksiazki[ksiazka] = 1
         else:
             self.ksiazki[ksiazka] += 1
         return "True"
     
-    def wypozycz_ksiazke(self,czytelnik, tytul):
+    def wypozycz_ksiazke(self,czytelnik: Czytelnik, tytul: str):
         if not self.pobierz_czytelnika(czytelnik) and self.sprawdz_stan(tytul):
-            self.czytelnicy[czytelnik] = {tytul: 1}
+            self.czytelnicy[czytelnik.imie] = [tytul]
             ksiazka = self.ksiazka_po_tytule(tytul)
             self.ksiazki[ksiazka] -= 1
             return "True"
 
         if self.sprawdz_stan(tytul) and not self.sprawdz_czy_ta_sama(czytelnik, tytul) and not self.sprawdz_czy_trzy(czytelnik):
-            self.czytelnicy[czytelnik][tytul] = 1
+            self.czytelnicy[czytelnik.imie].append(tytul)
             ksiazka = self.ksiazka_po_tytule(tytul)
             self.ksiazki[ksiazka] -= 1
             return "True"
         else:
             return "False"
 
-    def oddaj_ksiazke(self,czytelnik, tytul):
+    def oddaj_ksiazke(self,czytelnik: Czytelnik, tytul: str):
         if not self.pobierz_czytelnika(czytelnik):
             return "False"
-        if tytul not in self.czytelnicy[czytelnik]:
-            return "False"
-        if self.czytelnicy[czytelnik][tytul] == 0:
+        if tytul not in self.czytelnicy[czytelnik.imie]:
             return "False"
         else:
             ksiazka = self.ksiazka_po_tytule(tytul)
             self.ksiazki[ksiazka] += 1
-            self.czytelnicy[czytelnik][tytul] = 0
+            self.czytelnicy[czytelnik.imie].remove(tytul)  
             return "True"
 
 
@@ -89,7 +87,7 @@ class Biblioteka:
 
 def main():
     liczba_akcji = input()
-    biblioteka = Biblioteka({},{})
+    biblioteka = Biblioteka()
 
     for _ in range(int(liczba_akcji)):
         dane_wejsciowe = eval(input())
@@ -97,9 +95,11 @@ def main():
             ksiazka = Ksiazka(dane_wejsciowe[1], dane_wejsciowe[2], int(dane_wejsciowe[3]))
             wynik = biblioteka.dodaj_ksiazke(ksiazka)
         elif dane_wejsciowe[0] == "wypozycz":
-            wynik = biblioteka.wypozycz_ksiazke(dane_wejsciowe[1], dane_wejsciowe[2])
+            czytelnik = Czytelnik(dane_wejsciowe[1])
+            wynik = biblioteka.wypozycz_ksiazke(czytelnik, dane_wejsciowe[2])
         else:
-            wynik = biblioteka.oddaj_ksiazke(dane_wejsciowe[1], dane_wejsciowe[2])
+            czytelnik = Czytelnik(dane_wejsciowe[1])
+            wynik = biblioteka.oddaj_ksiazke(czytelnik, dane_wejsciowe[2])
         print(wynik)
 
         
